c4133857f4668bb57e6d7c67d83601d3ccdf6003
K2G90 <clw.jr90@gmail.com>
2021-10-29 15:52:24 -0400

    first component upload

---
+++ component-example.js
@@ -0,0 +1,10 @@
+// Just a quick example on how to create
+// a function in component in React.
+
+function NavBar() {
+    return(
+        <nav>
+            <h1>Pet App</h1>
+        </nav> 
+    )
+}
